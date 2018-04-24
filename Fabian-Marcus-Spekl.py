0e6d1019ccce72218a59950812a203c70bd4fe2f
Mockke <38683121+Mockke@users.noreply.github.com>
2018-04-24 10:27:19 +0200

    ändring på början

--- Fabian-Marcus-Spekl.py
+++ Fabian-Marcus-Spekl.py
@@ -14,12 +14,22 @@ Agility=0
 "def gamble"
 
 name = input("Hello there stranger! What is your name? ")
-print("Oh, Hello "+name+"! ")
+print("Oh, Hello "+name+"! My name is Gregor! Nice to meet you!")
 
-race = input("What species are you? (Barr/Hagar/Bengan)")
-print("Wow, you are a "+str(race))
+race = input('''Where are you from? Do you belong to any of the kingdoms?
+ 1 - Barr: They are known for their agility + hidden talant
+ 2 - Hagar: They are known for their health + hidden talant
+ 3 - Bengt: They are known for their strenght + hidden talant''')
 
-if race==Barr:
+
+
+if race==1:
     Health+=15
     Strenght+=5
     Agility+=20
+    print("Oh, so you are a Barr?? Wonderfull!")
+elif race==Hagar:
+    Health+=20
+    Strenght+=5
+    Agility+=15
+
