eb4e8583d15886290fffe48886671728c9d11781
Mockke <38683121+Mockke@users.noreply.github.com>
2018-04-24 09:31:52 +0200

    Race

--- Fabian-Marcus-Spekl.py
+++ Fabian-Marcus-Spekl.py
@@ -3,6 +3,12 @@
 import time
 import random
 
+Health=0
+Strenght=0
+Agility=0
+
+
+
 "def Fight"
 
 "def gamble"
@@ -12,3 +18,8 @@ print("Oh, Hello "+name+"! ")
 
 race = input("What species are you? (Barr/Hagar/Bengan)")
 print("Wow, you are a "+str(race))
+
+if race==Barr:
+    Health+=15
+    Strenght+=5
+    Agility+=20
